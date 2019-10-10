4d82c81c442dc088082a6194bca9d01935787425
Jack Laurence Garay <lawrencetecho@gmail.com>
2019-10-10 05:54:01 +0000

    Some fixes

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+require('dotenv').config({ path: './.env' });
 var proxy= require('express-http-proxy');
 var app = require('express')();
+var port = process.env.PORT || 7000;
+
+if (!process.env.HOST) 
+  throw new Error("You have to define ACTUAL_HOST in your environment variables.");
+
+var host = process.env.HOST;
+console.log('A proxy for ', host, ' is running');
+console.log('Running at: ', 'http://localhost:'.concat(port));
  
-app.use('/', proxy(process.env.ACTUAL_HOST));
+app.use('/', proxy(host));
+app.listen(port);
